12b243e9f531ad3e15f3038c386b5504c07c4550
helloitsliam <liamcleary@msn.com>
2018-03-21 19:10:24 -0400

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,1 +1,8 @@
-Write-Host "This is a test PowerShell Script that does no harm at all" -ForegroundColor Green -BackgroundColor DarkGray
+
+function WriteMessage()
+{
+    Write-Host "This is a test PowerShell Script that does no harm at all" -ForegroundColor Green -BackgroundColor DarkGray
+}
+
+WriteMessage
+
